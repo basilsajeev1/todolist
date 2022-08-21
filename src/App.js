7fbf429df46da3fe51328ff5bffcbc95390623af
Basil Sajeev <basilsajeev1@gmail.com>
2022-08-21 12:34:10 +0530

    displayed todos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [toDos,settoDos]=useState([])
+  let [task,setTask]=useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      
+      <div className="input">
+        <input type="text" placeholder="🖊️ Add item..." value={task} onChange={(e)=>{setTask(e.target.value)}}/>
+        <i className="fas fa-plus" onClick={()=>{settoDos([...toDos,task])}}></i>
+      </div>
+      {toDos.map((value)=>{
+        return(
+
+      <div className="todos">
+        <div className="todo">
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p>{value}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+      </div>
+        )
+      })
+     }
     </div>
   );
 }
