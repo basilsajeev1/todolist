f4be465a2918cc020c9c41298752f5341db3c4c8
Basil Sajeev <basilsajeev1@gmail.com>
2022-08-22 11:53:03 +0530

    Finished with option to add and delete tasks

--- src/App.js
+++ src/App.js
@@ -13,19 +13,28 @@ function App() {
       
       <div className="input">
         <input type="text" placeholder="🖊️ Add item..." value={task} onChange={(e)=>{setTask(e.target.value)}}/>
-        <i className="fas fa-plus" onClick={()=>{settoDos([...toDos,task])}}></i>
+        <i className="fas fa-plus" onClick={()=>{settoDos([...toDos,{id:Date.now(), text:task}])}}></i>
       </div>
-      {toDos.map((value)=>{
+      {toDos.map((obj)=>{
         return(
 
       <div className="todos">
         <div className="todo">
           <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>{value}</p>
+            
+            <p>{obj.text}</p>
           </div>
           <div className="right">
-            <i className="fas fa-times"></i>
+            <i id={obj.id} className="fas fa-times" onClick={(e)=>{
+              console.log(e.target.id)
+              console.log(obj.id)
+              let index= toDos.findIndex(obj=>{return obj.id==e.target.id})
+              console.log(index)
+              if (index !== -1) {
+                toDos.splice(index, 1);
+                settoDos([...toDos]);
+              }
+            }}></i>
           </div>
         </div>
       </div>
